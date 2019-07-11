bb0460384c11f31e9da32e77c86ededb7bf0a5d7
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:43:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove()
+
+var h1 = document.createElement("h1");
+h1.id = "victory";
+h1.innerHTML = "Roxanne is the champion"
+document.body.appendChild(h1);
+
+let newHeader = document.querySelector("h1");
