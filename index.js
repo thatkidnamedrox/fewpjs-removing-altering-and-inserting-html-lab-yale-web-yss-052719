aaf1d173c4f0aa6d38e25d0de384dd818d240239
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 main.remove()
 
 var h1 = document.createElement("h1");
-document.body.appendChild(h1);
 h1.id = "victory";
+document.body.appendChild(h1);
+
 let newHeader = document.querySelector("h1");
